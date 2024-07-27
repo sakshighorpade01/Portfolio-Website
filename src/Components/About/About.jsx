62d0e5ba4a9f2d7d682c0a1939ee74b5eb3e0138
Sakshi <sakshighorpade3011@gmail.com>
2024-07-27 15:00:09 +0530

    Updated files

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import './About.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
-import profile_img from '../../assets/about_profile.svg'
+// import profile_img from '../../assets/about_profile.svg'
+import myprofile from '../../assets/myprofile.png'
 
 const About = () =>{
     return (
@@ -12,22 +13,22 @@ const About = () =>{
             </div>
             <div className="about-sections">
                 <div className="about-left">
-                    <img src={profile_img} alt="" />
+                    <img src={myprofile} alt="" />
                 </div>
                 <div className="about-right">
                     <div className="about-para">
-                        <p>I am an experienced Frontend Developer with over one year of professional experience</p>
-                        <p>My passion for Frontend development is not only for job purpose but i enjoy a lot from this professional field</p>
+                        <p>I am a Web Developer with some experience</p>
+                        <p>My passion for Web development is not only for job purpose but i enjoy a lot from this professional field</p>
                     </div>
                     <div className="about-skills">
                         <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"50%"}}/></div>
-                        <div className="about-skill"><p>React JS</p><hr style={{width:"70%"}}/></div>
                         <div className="about-skill"><p>Javascript</p><hr style={{width:"60%"}}/></div>
-                        <div className="about-skill"><p>Next JS</p><hr style={{width:"50%"}}/></div>
+                        <div className="about-skill"><p>React JS</p><hr style={{width:"70%"}}/></div>
+                        <div className="about-skill"><p>Python</p><hr style={{width:"50%"}}/></div>
                     </div>
                 </div>
             </div>
-            <div className="about-achievements">
+            {/* <div className="about-achievements">
                 <div className="about-achievement">
                     <h1>10+</h1>
                     <p>YEARS OF EXPERIENCE</p>
@@ -42,7 +43,7 @@ const About = () =>{
                     <h1>15+</h1>
                     <p>HAPPY CLIENTS</p>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
+// import profile_img from '../../assets/profile_img.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-// import myprofile from '../../assets/myprofile.jpg'
+import myprofile from '../../assets/myprofile.png'
 
 
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-        <img  src={profile_img} alt="" />
-        <h1><span>I'm Sakshi Ghorpade,</span>Frontend Developer based in Pune</h1>
-        <p>I am frontend developer from Pune , Maharashtra with 1 year of experience</p>
+        <img  src={myprofile} alt="" />
+        <h1><span>I'm Sakshi Ghorpade,</span>Web Developer in Pune</h1>
+        <p>I am web developer from Pune , Maharashtra</p>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
             <div className="hero-resume">My resume</div>
